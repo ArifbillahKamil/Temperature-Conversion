7263747c5b76af84d0de712f0e0922a7c20b06d5
Muhammad Arifbillah Kamil <163496313+ArifbillahKamil@users.noreply.github.com>
2024-08-15 11:56:46 +0700

    Add files via upload
    JavaScript

---
+++ index.js
@@ -0,0 +1,22 @@
+const inputText = document.getElementById("inputText");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelcius = document.getElementById("toCelcius");
+const result = document.getElementById("result");
+let temp;
+
+function convert(){
+    if(toFahrenheit.checked){
+        temp = Number(inputText.value);
+        temp = temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + `°F`
+    }
+    else if(toCelcius.checked){
+        temp = Number(inputText.value);
+        temp = (temp - 32) * (5 / 9);
+        result.textContent = temp.toFixed(1) + `°C`
+    }
+    else{
+        result.textContent = `Select a Unit`;
+    }
+
+}
